3a24f1a8d79d4d4d71bda5cdca99c0323bcec8d5
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-05-08 05:52:18 +0800

    Updated

--- answers.php
+++ answers.php
@@ -9,7 +9,7 @@ require_once('config.php')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Answers</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -27,17 +27,17 @@ require_once('config.php')
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="index.php" class="nav-link active" aria-current="page">
+                    <a href="index.php" class="nav-link text-white">
                         Form
                     </a>
                 </li>
                 <li>
-                    <a href="families.php" class="nav-link text-white">
+                    <a href="families.php"  class="nav-link text-white">
                         Families
                     </a>
                 </li>
                 <li>
-                    <a href="answers.php" class="nav-link text-white">
+                    <a href="answers.php" class="nav-link active" aria-current="page">
                         Answers
                     </a>
                 </li>
@@ -49,7 +49,6 @@ require_once('config.php')
                     <strong>Admin</strong>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                    <!-- <li><a class="dropdown-item" href="#">New project...</a></li> -->
                     <li><a class="dropdown-item" href="#">Settings</a></li>
                     <li><a class="dropdown-item" href="#">Profile</a></li>
                     <li>
