c874b13ccf0ebd92749e24949a7618d51435ca9d
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-05-08 15:49:00 +0800

    Updated

--- handle_submit_form.php
+++ handle_submit_form.php
@@ -1,23 +1,55 @@
 <?php
 require_once('config.php');
 require_once('global.php');
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+$success_msg = null;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $post = validate_post_data($_POST);
     $fields = "id INT AUTO_INCREMENT PRIMARY KEY, ";
-    foreach($post as $key => $value) {
-        if(is_array($post[$key])) {
-            $fields.= "$key TEXT(5000), ";
+    $keys = [];
+    foreach ($post as $key => $value) {
+        if (is_array($post[$key])) {
+            $fields .= "$key TEXT(5000), ";
         } else {
-            $fields.= "$key TEXT(3000), ";
+            $fields .= "$key TEXT(3000), ";
         }
+
+        $keys[] = $key;
     }
 
 
     $sql = "CREATE TABLE IF NOT EXISTS form_records (" . $fields . "date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
 
     $result = mysqli_query($conn, $sql);
-    if(!$result) {
-        die("Error creating table: ". mysqli_error($conn));
+    if (!$result) {
+        die("Error creating table: " . mysqli_error($conn));
+        exit;
     }
+
+
+    // insert sql
+    $insertSql = "INSERT INTO form_records (" . implode(", ", $keys) . " ) VALUES (";
+    $i =  0;
+    $length = count($keys);
+    foreach ($post as $key => $value) {
+        if (is_array($post[$key])) {
+            $insertSql .= "'" . trim(implode(", ", $post[$key])) . "'";
+        } else {
+            $insertSql .= "'" . trim($post[$key]) . "'";
+        }
+        $i++;
+        if ($i < $length) {
+            $insertSql .= ", ";
+        }
+    }
+
+    $insertSql .= ")";
+
+    $result = mysqli_query($conn, $insertSql);
+    if (!$result) {
+        die("Error inserting data: " . mysqli_error($conn));
+        exit;
+    }
+
+    $success_msg = "Form submitted successfully";
+    
 }
